4e4b6aed81d7bc2b0f4069b94c6a47b569466523
sssssungs <32057999+sssssungs@users.noreply.github.com>
2020-04-08 12:58:47 +0900

    add

---
+++ sungsikTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sungsikTodo
+//
+//  Created by Randy Won on 2020/04/08.
+//  Copyright © 2020 Randy Won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
